0a1fcc95b052e5dab34b6813a2cdd4c844b3276d
Prateek Rastogi <p.rastogi@outlook.com>
2017-08-09 00:40:06 +0530

    Modified version UWPStyle to work according to standard preferences

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,9 @@ import Header from '../components/Header'
 import Submit from '../components/Submit'
 import PostList from '../components/PostList'
 import withData from '../lib/withData'
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
 
 export default withData((props) => (
   <App>
-    <Title>My page</Title>
     <Header pathname={props.url.pathname} />
     <Submit />
     <PostList />
