64314a444680d28dc0bd25cb0e21471417ccfd08
Prateek Rastogi <p.rastogi@outlook.com>
2017-08-09 20:07:36 +0530

    Added grid styled to help with responsive webdesign

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,34 @@ import Submit from '../components/Submit'
 import PostList from '../components/PostList'
 import withDataAndRootTheme from '../lib/withDataAndRootTheme'
 import styled from 'styled-components'
+import Button from 'react-uwp/Button'
+import { Flex, Box } from 'grid-styled'
 
-const Title = styled.h1`
-  color: red;
+const StyledButton = styled(Button)`
   font-size: 50px;
+  color: red !important;
+  background: blue !important;
 `
 
 export default withDataAndRootTheme((props) => (
   <App>
-    <Title>My page</Title>
-    <Header pathname={props.url.pathname} />
-    <Submit />
-    <PostList />
+    <Flex>
+      <Box width={[
+        1/2
+      ]} px={2}>
+        <StyledButton icon='RatingStarFillZeroWidthLegacy' tooltip='click here'>Button </StyledButton>
+      </Box>
+      <Box width={[
+        1/2,
+        1/3,
+        1/4,
+        1/6
+      ]} px={2}>
+        <StyledButton icon='RatingStarFillZeroWidthLegacy' tooltip='click here'>Button </StyledButton>
+        <Header pathname={props.url.pathname} />
+        <Submit />
+        <PostList />
+      </Box>
+    </Flex>
   </App>
 ))
