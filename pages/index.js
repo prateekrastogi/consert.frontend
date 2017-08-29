b0b184d32a4ee30cf48969e8603c2c239a2703c2
Prateek Rastogi <p.rastogi@outlook.com>
2017-08-29 22:25:48 +0530

    Changed the Index class to React ClassType and wired it with withdata to map props from state store

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import React from 'react'
 import withData from '../lib/withData'
 import App from '../components/App'
 import { Button } from 'semantic-ui-react'
 
-export default withData((props) => (
-  <App>
-    <Button> echo</Button>
-  </App>
-))
+class Index extends React.Component {
+  render () {
+    return (<App>
+      <Button> echo</Button>
+    </App>)
+  }
+}
+
+export default withData(Index)
