de41344ef97d268707f186c6d18263491f43e991
Prateek Rastogi <p.rastogi@outlook.com>
2017-09-03 20:41:45 +0530

    Fixed the problem of icons not showing in semantic-ui theme

--- components/App.js
+++ components/App.js
@@ -3,6 +3,7 @@ import stylesheet from 'styles/semantic.min.css'
 export default ({children}) => (
   <main>
     <div>
+      <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/components/icon.min.css' />
       <style dangerouslySetInnerHTML={{__html: stylesheet}} />
       {children}</div>
   </main>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import withData from '../lib/withData'
 import App from '../components/App'
-import { Sidebar, Menu } from 'semantic-ui-react'
+import { Sidebar, Menu, Icon } from 'semantic-ui-react'
 
 class Index extends React.Component {
   render () {
     return (<App>
       <Sidebar as={Menu} animation='overlay' width='thin' visible icon='labeled' vertical inverted>
-        <Menu.Item name='' />
+        <Menu.Item name='home'>
+          <Icon name='home' />
+            Home
+        </Menu.Item>
+        <Menu.Item name='gamepad'>
+          <Icon name='gamepad' />
+            Games
+        </Menu.Item>
+        <Menu.Item name='camera'>
+          <Icon name='camera' />
+            Channels
+        </Menu.Item>
       </Sidebar>
     </App>
     )
