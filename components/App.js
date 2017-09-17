b48a7098507df3e55eb7ce7700c91fbc67a8cac4
Prateek Rastogi <p.rastogi@outlook.com>
2017-09-18 02:31:52 +0530

    Adding Google tag manager

--- components/App.js
+++ components/App.js
@@ -1,10 +1,27 @@
+import React from 'react'
 import stylesheet from 'styles/semantic.min.css'
+import Head from 'next/head'
 
-export default ({children}) => (
-  <main>
-    <div>
-      <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/components/icon.min.css' />
-      <style dangerouslySetInnerHTML={{__html: stylesheet}} />
-      {children}</div>
-  </main>
-)
+export default class App extends React.Component {
+  render () {
+    return (
+      <main>
+        <Head>
+          <link rel='stylesheet prefetch' href={process.env.SEMANTIC_UI_ICON_URL} />
+          <style dangerouslySetInnerHTML={{__html: stylesheet}} />
+          <style dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GTM_CODE}');`}} />
+        </Head>
+
+        <div>
+          {this.props.children}</div>
+
+        <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_CODE}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
+      </main>
+    )
+  }
+}
