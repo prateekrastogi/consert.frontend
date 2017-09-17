c005aed927a59383be27698ee6f84beb1e49e27a
Prateek Rastogi <p.rastogi@outlook.com>
2017-09-18 02:43:22 +0530

    Added Google tag manager

--- components/App.js
+++ components/App.js
@@ -8,7 +8,9 @@ export default class App extends React.Component {
       <main>
         <Head>
           <link rel='stylesheet prefetch' href={process.env.SEMANTIC_UI_ICON_URL} />
+
           <style dangerouslySetInnerHTML={{__html: stylesheet}} />
+
           <style dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -16,11 +18,15 @@ export default class App extends React.Component {
         })(window,document,'script','dataLayer','${process.env.GTM_CODE}');`}} />
         </Head>
 
-        <div>
-          {this.props.children}</div>
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_CODE}"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }} />
 
-        <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_CODE}"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
+        <div>
+          {this.props.children}
+        </div>
+
       </main>
     )
   }
