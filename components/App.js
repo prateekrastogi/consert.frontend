7bd29f00e7eb7b2b0d2f6fb6115b4c546165191d
Prateek Rastogi <p.rastogi@outlook.com>
2017-09-20 01:00:37 +0530

    Added browser fingerprinting to uniquely identify users

--- components/App.js
+++ components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import stylesheet from 'styles/semantic.min.css'
 import Head from 'next/head'
 import Cookies from 'universal-cookie'
+import Fingerprint from 'fingerprintjs2'
 
 export default class App extends React.Component {
   render () {
@@ -12,16 +13,18 @@ export default class App extends React.Component {
 
           <style dangerouslySetInnerHTML={{__html: stylesheet}} />
 
-          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.GTM_CODE}');`}} />
+        })(window,document,'script','dataLayer','${process.env.GTM_CODE}');`
+          }} />
         </Head>
 
         <noscript dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_CODE}"
-                    height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          __html: `<iframe src='https://www.googletagmanager.com/ns.html?id=${process.env.GTM_CODE}EXTERNAL_FRAGMENT'
+          '0'       '0'ght="0"'display:none;visibility:hidden'isibility:hidden"></iframe>`
         }} />
 
         <div>
@@ -33,6 +36,12 @@ export default class App extends React.Component {
   }
 
   componentDidMount () {
+    const cookies = new Cookies()
 
+    if (!cookies.get('browserId')) {
+      new Fingerprint().get((fingerprint) => {
+        cookies.set('browserId', fingerprint, {maxAge: process.env.COOKIE_TTL})
+      })
+    }
   }
 }
