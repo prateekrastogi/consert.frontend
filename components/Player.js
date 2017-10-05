ad3943586f43f1474d24230b4dc25f9737067af3
Prateek Rastogi <p.rastogi@outlook.com>
2017-10-06 01:17:33 +0530

    Updated to react 16 and nextjs 4 beta branch

--- components/Player.js
+++ components/Player.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 
 export default class Player extends React.Component {
-  render(){
-    <Player />
+  render () {
+    return (<Player />)
   }
 }
