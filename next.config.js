9ac08bde13cb00a07028a2f0cf95097938581103
Prateek Rastogi <p.rastogi@outlook.com>
2018-03-27 13:19:51 +0530

    added bundle analyzer plugin in config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const compose = require('next-compose')
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
 const nextConfig = {
   webpack: (config) => {
@@ -20,4 +21,19 @@ const nextConfig = {
   }
 }
 
-module.exports = compose([nextConfig])
+const bundleAnalyzerConfig = {
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../../bundles/server.html'
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html'
+    }
+  }
+}
+
+module.exports = compose([[withBundleAnalyzer, bundleAnalyzerConfig], nextConfig])
