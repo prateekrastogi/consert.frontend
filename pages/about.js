8d73e44166c902150d70f473bb638635b25c7f8b
Prateek Rastogi <p.rastogi@outlook.com>
2018-04-05 16:35:32 +0530

    modified the base example to illustrate css file splitting

--- pages/about.js
+++ pages/about.js
@@ -1,37 +1,13 @@
 import withData from '../lib/withData'
+import { Icon } from 'semantic-ui-react'
+import App from '../components/App'
+import '../semantic/dist/components/icon.min.css'
 
 function About (props) {
   return (
-    <article>
-      <div>{props.children}</div>
-      <h1>The Idea Behind This Example</h1>
-      <p>
-        <a href='http://dev.apollodata.com'>Apollo</a> is a GraphQL client that allows you to easily query the exact
-        data
-        you need from a GraphQL server. In addition to fetching and mutating data, Apollo analyzes your queries and
-        their
-        results to construct a client-side cache of your data, which is kept up to date as further queries and mutations
-        are run, fetching more results from the server.
-      </p>
-      <p>
-        In this simple example, we integrate Apollo seamlessly with <a href='https://github.com/zeit/next.js'>Next</a>
-        by
-        wrapping our pages inside a <a href='https://facebook.github.io/react/docs/higher-order-components.html'>higher-order
-        component (HOC)</a>. Using the HOC pattern we're able to pass down a central store of query result data created
-        by
-        Apollo into our React component hierarchy defined inside each page of our Next application.
-      </p>
-      <p>
-        On initial page load, while on the server and inside getInitialProps, we invoke the Apollo method, <a
-          href='http://dev.apollodata.com/react/server-side-rendering.html#getDataFromTree'>getDataFromTree</a>. This
-        method
-        returns a promise; at the point in which the promise resolves, our Apollo Client store is completely
-        initialized.
-      </p>
-      <p>
-        This example relies on <a href='/auth/spotify'>Auth</a> for its GraphQL backend.
-      </p>
-    </article>
+    <App>
+      <Icon name='world' />
+    </App>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,13 @@ import Head from 'next/head'
 import React from 'react'
 import withData from '../lib/withData'
 import App from '../components/App'
-import { Popup, Image, Menu, Icon } from 'semantic-ui-react'
-import '../semantic/dist/components/icon.min.css'
+import { Button } from 'semantic-ui-react'
+import '../semantic/dist/components/button.min.css'
 
 class Index extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {active: false}
-  }
-
   render () {
-    const { active } = this.state
     return (<App>
-
-      <Menu inverted size='massive' widths={3}>
-
-        <Menu.Item name='logo'>
-          <Popup inverted trigger={<Image alt='consert' src={require('../assets/logo.svg')} size='tiny' />} content='consert' />
-        </Menu.Item>
-
-        <Menu.Item name='explore' position='right'>
-          <Popup inverted trigger={<Image alt='explore' src='../assets/explore.svg' size='tiny' />} content='explore' />
-        </Menu.Item>
-
-      </Menu>
-      <Icon name='world' />
+      <Button>Button</Button>
     </App>
 
     )
