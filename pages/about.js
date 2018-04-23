ca1cf6209f673622f1401420ef6880c0a1da8f24
Prateek Rastogi <p.rastogi@outlook.com>
2018-04-23 13:07:42 +0530

    preparing index page for actual integrations

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,27 @@
 import withData from '../lib/withData'
-import { Icon } from 'semantic-ui-react'
+import { Icon, Button } from 'semantic-ui-react'
 import App from '../components/App'
+import Player from '../components/Player'
 import '../semantic/dist/components/icon.min.css'
+import '../semantic/dist/components/button.min.css'
 
 function About (props) {
+  const videoJsOptions = {
+    techOrder: ['youtube'],
+    autoplay: false,
+    controls: true,
+    muted: true,
+    sources: [{
+      src: 'https://www.youtube.com/watch?v=jiLkBxw2pbs',
+      type: 'video/youtube'
+    }]
+  }
+
   return (
     <App>
       <Icon name='world' />
+      <Player {...videoJsOptions} />
+      <Button>Button</Button>
     </App>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,9 @@
-import Link from 'next/link'
-import Head from 'next/head'
 import React from 'react'
 import withData from '../lib/withData'
 import App from '../components/App'
-import Player from '../components/Player'
-import { Button } from 'semantic-ui-react'
-import '../semantic/dist/components/button.min.css'
-
 class Index extends React.Component {
   render () {
-    const videoJsOptions = {
-      techOrder: ['youtube'],
-      autoplay: false,
-      controls: true,
-      muted: true,
-      sources: [{
-        src: 'https://www.youtube.com/watch?v=jiLkBxw2pbs',
-        type: 'video/youtube'
-      }]
-    }
-
-    return (<App>
-      <Player {...videoJsOptions} />
-      <Button>Button</Button>
-    </App>
+    return (<App />
 
     )
   }
