e5835dfdc403275dfad9f59d5a2e1572ff0f3052
Prateek Rastogi <p.rastogi@outlook.com>
2018-06-09 02:07:10 +0530

    added sharemodal skelton

---
+++ components/Player/ShareModal.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import {Modal} from 'semantic-ui-react'
+import '../semantic/dist/components/modal.min.css'
+
+class ShareModal extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {isShareButtonClicked: false}
+  }
+
+  componentWillMount () {
+
+  }
+
+  componentDidMount () {
+
+  }
+
+  componentWillReceiveProps (nextProps) {
+
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+
+  }
+
+  componentWillUnmount () {
+
+  }
+
+  render () {
+    return (
+      <div />
+    )
+  }
+}
+
+export default ShareModal
