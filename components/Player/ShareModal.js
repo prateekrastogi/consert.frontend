656e725e74ab21076a7ca46a20bc9efdceb22f9c
Prateek Rastogi <p.rastogi@outlook.com>
2018-06-15 19:01:43 +0530

    EOD commit

--- components/Player/ShareModal.js
+++ components/Player/ShareModal.js
@@ -1,46 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Modal} from 'semantic-ui-react'
-import '../semantic/dist/components/modal.min.css'
+import '../../semantic/dist/components/modal.min.css'
 
-class ShareModal extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {isShareButtonClicked: false}
-  }
-
-  componentWillMount () {
-
-  }
-
-  componentDidMount () {
-
-  }
-
-  componentWillReceiveProps (nextProps) {
-
-  }
-
-  shouldComponentUpdate (nextProps, nextState) {
-
-  }
-
-  componentWillUpdate (nextProps, nextState) {
-
-  }
-
-  componentDidUpdate (prevProps, prevState) {
-
-  }
-
-  componentWillUnmount () {
-
-  }
-
-  render () {
-    return (
-      <div />
-    )
-  }
+const ShareModal = () => {
+  return (
+    <div />
+  )
 }
 
 export default ShareModal
---
+++ components/PlayerCard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class PlayerCard extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isMobileOS: false,
+      mediaSrc: '',
+      shouldOpenShareModal: false
+    }
+  }
+
+  componentWillMount () {
+
+  }
+
+  componentDidMount () {
+
+  }
+
+  componentWillReceiveProps (nextProps) {
+
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+
+  }
+
+  componentWillUnmount () {
+
+  }
+
+  render () {
+    return (
+      <div />
+    )
+  }
+}
+
+export default PlayerCard
