eb04ce1455ba3031e1b74d2cc870160869adb0ab
Prateek Rastogi <p.rastogi@outlook.com>
2018-08-06 16:55:49 +0530

    integrated first graphql query to fetch data from server side :-)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+const GET_APP_METADATA_TAGS = gql`
+query{
+  appMetadata {
+    appMetadataGetTags {
+      edges {
+        node
+      }
+    }
+  }
+}
+`
 
 class Index extends React.Component {
   render () {
     return (
-      <main />
+      <Query query={GET_APP_METADATA_TAGS} >
+        {({loading, error, data}) => {
+          if (loading) return 'Loading...'
+          if (error) return `Error! ${error.message}`
+          return (<p>{JSON.stringify(data)}</p>)
+        }
+        }
+      </Query>
     )
   }
 }
