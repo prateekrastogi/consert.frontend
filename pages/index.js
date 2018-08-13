40da2e04840437fb8b8563b6944cbd810bf1cdc5
Prateek Rastogi <p.rastogi@outlook.com>
2018-08-13 13:32:18 +0530

    properly passing required params to the query

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 
 const GET_APP_METADATA_TAGS = gql`
-query{
+query appMetadataGetTags($context: JSON){
   appMetadata {
-    appMetadataGetTags {
+    appMetadataGetTags(context: $context) {
       edges {
         node
       }
@@ -14,10 +14,16 @@ query{
 }
 `
 
+const queryVariables = {
+  context: {
+    serialized: true
+  }
+}
+
 class Index extends React.Component {
   render () {
     return (
-      <Query query={GET_APP_METADATA_TAGS} >
+      <Query query={GET_APP_METADATA_TAGS} variables={queryVariables} >
         {({loading, error, data}) => {
           if (loading) return 'Loading...'
           if (error) return `Error! ${error.message}`
